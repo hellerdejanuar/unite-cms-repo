cf9b7e31832f4cf022d8420a6b3acfe894046209
Mauricio Heller <hellerdejanuar@gmail.com>
2023-08-10 12:24:59 -0300

    Refactored STD_EVENT params to its cwn config file

---
+++ uniteCMS/src/api/event/config/event_params.js
@@ -0,0 +1,44 @@
+const STD_PARTICIPANTS_FIELDS = {
+  populate: {
+    profile_image: {
+      fields: ['formats']
+    }
+  },
+  fields: [
+    'id',
+    'username',
+    'phone'
+  ]
+}
+
+const STD_EVENT_HOST_FIELDS = {
+  populate: {
+    profile_image: {
+      fields: ['formats']
+    }
+  },
+  fields: [
+  'id',
+  'username',
+  'phone',
+  ]
+}
+
+module.exports = { 
+  STD_POPULATE_EVENT: {
+    image: { fields: ['formats'] },
+    event_host: STD_EVENT_HOST_FIELDS, 
+    participants: STD_PARTICIPANTS_FIELDS,
+  },
+
+  STD_EVENT_FIELDS: [
+    'name', 
+    'id',
+    'max_people',
+    'people_count',
+    'event_datetime',
+    'location',
+    'config',
+    'description',
+  ],
+}
--- uniteCMS/src/api/event/services/event.js
+++ uniteCMS/src/api/event/services/event.js
@@ -1,143 +1,85 @@
-'use strict';
+"use strict";
 
 /**
  * event service
  */
 
-const { sanitize } = require('@strapi/utils');
+const { sanitize } = require("@strapi/utils");
 const { contentAPI } = sanitize;
+const { createCoreService } = require("@strapi/strapi").factories;
 
-const { createCoreService } = require('@strapi/strapi').factories;
+const {
+  STD_POPULATE_EVENT,
+  STD_EVENT_FIELDS,
+} = require("../config/event_params");
 
-const STD_PARTICIPANTS_FIELDS = {
-  populate: {
-    profile_image: {
-      fields: ['formats']
-    }
-  },
-  fields: [
-    'id',
-    'username',
-    'phone'
-  ]
-}
-const STD_EVENT_HOST_FIELDS = {
-  populate: {
-    profile_image: {
-      fields: ['formats']
-    }
-  },
-  fields: [
-  'id',
-  'username',
-  'phone',
-  ]
-}
-const STD_POPULATE_EVENT = {
-  image: { fields: ['formats'] },
-  event_host: STD_EVENT_HOST_FIELDS, 
-  participants: STD_PARTICIPANTS_FIELDS,
-}
-const STD_EVENT_FIELDS = [
-  'name', 
-  'id',
-  'max_people',
-  'people_count',
-  'event_datetime',
-  'location',
-  'config',
-  'description',
-]
-
-
-module.exports = createCoreService('api::event.event', ({strapi}) => ({
-
+module.exports = createCoreService("api::event.event", ({ strapi }) => ({
   async fetchPersonalEvents(id, ctx) {
-    const USERS_PERMISSIONS_SERVICE = 'plugin::users-permissions.user'
+    const USERS_PERMISSIONS_SERVICE = "plugin::users-permissions.user";
 
-    const POPULATE =  {
-      attending_events: { populate : STD_POPULATE_EVENT,
-                          fields: STD_EVENT_FIELDS, } , // TODO: pedir related imagenes de hosted y attending 
-      hosted_events: {  populate: STD_POPULATE_EVENT,
-                        fields: STD_EVENT_FIELDS }, 
-    }
-    
-    const PARAMS = { 
-      populate: POPULATE
-    }
-  
-    const contentType = strapi.contentType(USERS_PERMISSIONS_SERVICE)
-    const PersonalEvents_unclean = strapi.entityService.findOne(USERS_PERMISSIONS_SERVICE, id, PARAMS)
+    const POPULATE = {
+      attending_events: {
+        populate: STD_POPULATE_EVENT,
+        fields: STD_EVENT_FIELDS,
+      },
+      hosted_events: {
+        populate: STD_POPULATE_EVENT,
+        fields: STD_EVENT_FIELDS,
+      },
+    };
+
+    const PARAMS = {
+      populate: POPULATE,
+    };
+
+    const contentType = strapi.contentType(USERS_PERMISSIONS_SERVICE);
+    const PersonalEvents_unclean = strapi.entityService.findOne(
+      USERS_PERMISSIONS_SERVICE,
+      id,
+      PARAMS
+    );
     // @ts-ignore
-    const PersonalEvents_sanitized = await contentAPI.output(PersonalEvents_unclean, contentType, ctx.state.auth);
-  
-    return PersonalEvents_unclean // <--- TODO: Clean this data
-  },
-  
-  async fetchEventsFeed(id, ctx) {
-    const SERVICE = 'api::event.event'
-    const PARTICIPANTS_FIELDS = {
-      populate: {
-        profile_image: {
-          fields: ['formats']
-        }
-      },
-      fields: [
-        'id',
-        'username',
-        'phone'
-      ]
-    }
-    const EVENT_HOST_FIELDS = {
-      populate: {
-        profile_image: {
-          fields: ['formats']
-        }
-      },
-      fields: [
-      'id',
-      'username',
-      'phone',
-      ]
-    }
-    const POPULATE_EVENT = {
-      image: { fields: ['formats'] },
-      event_host: EVENT_HOST_FIELDS, 
-      participants: PARTICIPANTS_FIELDS,
-    }
-    const EVENT_FIELDS = [
-      'name', 
-      'id',
-      'max_people',
-      'people_count',
-      'event_datetime',
-      'location',
-      'config',
-      'description',
-    ]
-    const EVENT_FEED_FILTERS = {  
-      $not: {
-          event_host: { id: id }
-      }
-    }
-  
-    const PARAMS = { 
-      populate: POPULATE_EVENT,
-      fields: EVENT_FIELDS,
-      filters: EVENT_FEED_FILTERS
-    }
-  
-    const EventsFeed_unclean = await strapi.entityService.findMany(SERVICE, PARAMS)
-    const contentType = strapi.contentType(SERVICE)
-    const EventsFeed_sanitized = await contentAPI.output(EventsFeed_unclean, contentType, ctx.state.auth);
-    
-    return EventsFeed_sanitized
-  },
-  
-  async getHomePackage  (ctx) {
+    const PersonalEvents_sanitized = await contentAPI.output(
+      PersonalEvents_unclean,
+      contentType,
+      ctx.state.auth
+    );
 
-    const id = ctx.params.id
-    const authUser = ctx.state.user; // <- jwt 
+    return PersonalEvents_unclean; // <--- TODO: Sanitize !! this data
+  },
+
+  async fetchEventsFeed(id, ctx) {
+    const SERVICE = "api::event.event";
+
+    const EVENT_FEED_FILTERS = {
+      $not: {
+        event_host: { id: id },
+      },
+    };
+
+    const PARAMS = {
+      populate: STD_POPULATE_EVENT,
+      fields: STD_EVENT_FIELDS,
+      filters: EVENT_FEED_FILTERS,
+    };
+
+    const EventsFeed_unclean = await strapi.entityService.findMany(
+      SERVICE,
+      PARAMS
+    );
+    const contentType = strapi.contentType(SERVICE);
+    const EventsFeed_sanitized = await contentAPI.output(
+      EventsFeed_unclean,
+      contentType,
+      ctx.state.auth
+    );
+
+    return EventsFeed_sanitized;
+  },
+
+  async getHomePackage(ctx) {
+    const id = ctx.params.id;
+    const authUser = ctx.state.user; // <- jwt
 
     if (!authUser) {
       return ctx.unauthorized();
@@ -145,26 +87,26 @@ module.exports = createCoreService('api::event.event', ({strapi}) => ({
       return ctx.unauthorized();
     }
 
-    // ## Main function ---> 
+    // ## Main function --->
     const HomePackage = await Promise.all([
       // @ts-ignore
-      strapi.service('api::event.event').fetchPersonalEvents(id, ctx), 
+      strapi.service("api::event.event").fetchPersonalEvents(id, ctx),
       // @ts-ignore
-      strapi.service('api::event.event').fetchEventsFeed(id, ctx)
+      strapi.service("api::event.event").fetchEventsFeed(id, ctx),
     ])
-      .then(([ userData, eventsFeed ]) => {
+      .then(([userData, eventsFeed]) => {
         // Process PersonalEvents & Feed Data
-        return { 
-          username: userData.username || "" ,
+        return {
+          username: userData.username || "",
           attending_events: userData.attending_events || [],
           hosted_events: userData.hosted_events || [],
-          events_feed: eventsFeed || []
-        }
-    }).catch((err) => {
-      console.log( 'Error at: homePackage' + err )
-    })
-    
-    return HomePackage
-  } 
-  }
-));
+          events_feed: eventsFeed || [],
+        };
+      })
+      .catch((err) => {
+        console.log("Error at: homePackage" + err);
+      });
+
+    return HomePackage;
+  },
+}));
