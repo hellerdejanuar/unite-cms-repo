62a193e8285159a9fccd7fde504b5086b75d23d7
Mauricio Heller <hellerdejanuar@gmail.com>
2023-08-17 15:56:38 -0300

    Create event Automatic Host assign

--- uniteCMS/src/api/event/controllers/event.js
+++ uniteCMS/src/api/event/controllers/event.js
@@ -14,6 +14,17 @@ module.exports = createCoreController('api::event.event',
       } catch (err) {
         ctx.body = err
       }
+    },
+
+    async create(ctx) {
+      let request = JSON.parse(ctx.request.body.data);
+
+      request["event_host"] = { connect : [ctx.state.user] }
+
+      ctx.request.body.data = JSON.stringify(request)
+      // @ts-ignore
+      const response = await super.create(ctx);
+      return response
     }
   })
 );
